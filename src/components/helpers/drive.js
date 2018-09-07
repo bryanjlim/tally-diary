9cfaac7bd1c3996faa6b1ac8ed1b46cb6d940c7a
Bryan Lim <bryanjlim@outlook.com>
2018-09-07 09:43:25 -0700

    Post entry in drive helper

--- src/components/helpers/drive.js
+++ src/components/helpers/drive.js
@@ -15,8 +15,44 @@ export default class Drive {
                 }).error((err) => {
                     reject(err);
                 })
-        });
-        
+        });   
     }
 
+    static postEntry(entryData) {
+        const boundary = '-------314159265358979323846264';
+        const delimiter = "\r\n--" + boundary + "\r\n";
+        const end_request = "\r\n--" + boundary + "--";
+
+        const contentType = 'application/json';
+        const metadata = {
+            'name': entryData.title, // TODO: make file name the diary entry number
+            'mimeType': contentType,
+            'parents': ['appDataFolder']
+        };
+        const base64Data = btoa(JSON.stringify(entryData));
+        const multipartRequest =
+            // metadata request 
+            delimiter +
+            'Content-Type: application/json\r\n\r\n' +
+            JSON.stringify(metadata) +
+            delimiter +
+            // body content request 
+            'Content-Type: ' + contentType + '\r\n' +
+            'Content-Transfer-Encoding: base64\r\n' +
+            '\r\n' +
+            base64Data +
+            end_request;
+        const request = gapi.client.request({
+            'path': 'https://www.googleapis.com/upload/drive/v3/files',
+            'method': 'POST',
+            'params': {'uploadType': 'multipart'},
+            'headers': {
+              'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
+            },
+            'body': multipartRequest});
+        request.execute(function(arg) {
+            // arg returns false when an error occured 
+            console.log(arg);
+        });
+    }
 }
--- src/components/pages/add_entry/addEntry.js
+++ src/components/pages/add_entry/addEntry.js
@@ -1,5 +1,6 @@
 /* global gapi */
 import React, { Component } from 'react';
+import Drive from '../../helpers/drive';
 import Mood from '../../objects/mood/mood'; 
 import Weather from '../../objects/weather/weather'; 
 import Todo from '../../objects/todos/todo'; 
@@ -51,13 +52,8 @@ export class AddEntry extends Component {
     }
 
     addNewEntry(e) {
-        const boundary = '-------314159265358979323846264';
-        const delimiter = "\r\n--" + boundary + "\r\n";
-        const end_request = "\r\n--" + boundary + "--";
-
         e.preventDefault();
-
-        const entryData = {
+        Drive.postEntry({
             "title": this.state.title, 
             "date": this.state.date,
             "bodyText": this.state.bodyText,
@@ -65,39 +61,7 @@ export class AddEntry extends Component {
             "weather": this.state.weather, 
             "todos": this.state.todos,
             "mood": this.state.mood
-        };
-        const contentType = 'application/json';
-        const metadata = {
-            'name': this.state.title, // TODO: make file name the diary entry number
-            'mimeType': contentType,
-            'parents': ['appDataFolder']
-        };
-        const base64Data = btoa(JSON.stringify(entryData));
-        const multipartRequest =
-            // metadata request 
-            delimiter +
-            'Content-Type: application/json\r\n\r\n' +
-            JSON.stringify(metadata) +
-            delimiter +
-            // body content request 
-            'Content-Type: ' + contentType + '\r\n' +
-            'Content-Transfer-Encoding: base64\r\n' +
-            '\r\n' +
-            base64Data +
-            end_request;
-        const request = gapi.client.request({
-            'path': 'https://www.googleapis.com/upload/drive/v3/files',
-            'method': 'POST',
-            'params': {'uploadType': 'multipart'},
-            'headers': {
-              'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
-            },
-            'body': multipartRequest});
-        request.execute(function(arg) {
-            // arg returns false when an error occured 
-            console.log(arg);
         });
-
         this.setState({
             customTitle: '',
             date: new Date(),
