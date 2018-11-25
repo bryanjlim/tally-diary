0f06387ea0afe1e74677c616bfe0eb4e58e54b3a
bryanjlim <bryanlim@cs.washington.edu>
2018-11-24 22:07:01 -0800

    Undo ux mode

--- src/components/app.js
+++ src/components/app.js
@@ -125,7 +125,6 @@ class App extends Component {
       clientId: '577206274010-9oung4hgd77fij9e50kjbc32tlviai4e.apps.googleusercontent.com',
       discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
       scope: 'https://www.googleapis.com/auth/drive.appdata',
-      ux_mode: 'redirect',
     }).then(() => {
       that.updateSignInStatus();
       if (that.state.isSignedIn) {
